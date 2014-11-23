d2f77e74826701548fad816bee749f2318eb23da
Christopher McLean <havok2905@gmail.com>
2014-11-23 17:17:27 -0500

    worked on api class

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,7 @@
 require 'grape'
 require_relative './whitelist.rb'
+require_relative './parser.rb'
+require_relative './validator.rb'
 
 module Terminal
   class API < Grape::API
@@ -13,6 +15,10 @@ module Terminal
         requires :input, type: String, desc: 'the string being executed on the terminal'
       end
       post :run do
+        status 200
+        {
+
+        }
       end
 
       desc 'return if a given command is valid or not'
@@ -20,6 +26,15 @@ module Terminal
         requires :input, type: String, desc: 'the string being executed on the terminal'
       end
       post :validate do
+        status 200
+        parser    = Parser.new @input
+        whitelist = WhiteList.commands
+        validator = Validator.new whitelist, parser.parse
+
+        {
+          command: @input,
+          valid: validator.whitelisted?
+        }
       end
 
       desc 'return a list of whitelisted commands'
--- test/api_spec.rb
+++ test/api_spec.rb
@@ -13,40 +13,29 @@ describe Terminal::API do
   let (:validate)  { '/api/v1/command/validate' }
   let (:whitelist) { '/api/v1/command/whitelist' }
 
-  let(:success)    { '' }
-  let(:blacklist)  { '' }
-  let(:error)      { '' }
+  let (:command)   { 'ls | grep foobar' }
+
+  after do
+    response = JSON.parse last_response.body
+    expect(last_response.status).to eq(200)
+    expect(response.empty?).to be_falsey
+  end
 
   describe 'POST /api/v1/command/run' do
     it 'should be successful' do
-      post run, command: success
-    end
-
-    it 'should fail with none whitelisted commands' do
-      post run, command: blacklist
-    end
-
-    it 'should fail with actual system errors' do
-      post run, command: error
+      post run, input: command
     end
   end
 
   describe 'POST /api/v1/command/validate' do
     it 'should be a valid command is all commands are whitelisted' do
-      post validate, command: success
-    end
-
-    it 'should not be valid is a command is not whitelisted' do
-      post validate, command: blacklist
+      post validate, input: command
     end
   end
 
   describe 'GET /api/v1/command/whitelist' do
     it 'should return a list of whitelisted commands' do
       get whitelist
-      response = JSON.parse last_response.body
-      expect(last_response.status).to eq(200)
-      expect(response.empty?).to be_falsey
     end
   end
 
