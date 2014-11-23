12ce9f433ca79fb8fe6e0d3d566e161975c3d443
Christopher McLean <havok2905@gmail.com>
2014-11-23 02:25:06 -0500

    expanded on api test

--- test/api_spec.rb
+++ test/api_spec.rb
@@ -8,34 +8,41 @@ describe Terminal::API do
     Terminal::API
   end
 
-  let(:success)   { '' }
-  let(:blacklist) { '' }
-  let(:error)     { '' }
+  let (:run)       { '/api/v1/command/run' }
+  let (:validate)  { '/api/v1/command/validate' }
+  let (:whitelist) { '/api/v1/command/whitelist' }
+
+  let(:success)    { '' }
+  let(:blacklist)  { '' }
+  let(:error)      { '' }
 
   describe 'POST /api/v1/command/run' do
     it 'should be successful' do
-      post '/api/v1/command/run', command: success
+      post run, command: success
     end
 
     it 'should fail with none whitelisted commands' do
-      post '/api/v1/command/run', command: blacklist
+      post run, command: blacklist
     end
 
     it 'should fail with actual system errors' do
-      post '/api/v1/command/run', command: error
+      post run, command: error
     end
   end
 
   describe 'POST /api/v1/command/validate' do
     it 'should be a valid command is all commands are whitelisted' do
+      post validate, command: success
     end
 
     it 'should not be valid is a command is not whitelisted' do
+      post validate, command: blacklist
     end
   end
 
   describe 'GET /api/v1/command/whitelist' do
     it 'should return a list of whitelisted commands' do
+      get whitelist
     end
   end
 
