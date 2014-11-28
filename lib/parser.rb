5ace9d742404fef764186a38a788ef8da702c206
Christopher McLean <havok2905@gmail.com>
2014-11-28 17:50:04 -0500

    added comments and pseudo code to parse method

--- lib/parser.rb
+++ lib/parser.rb
@@ -16,19 +16,24 @@ module Terminal
       /^-{2}[a-zA-Z0-9]+={1}.*+$/.match(entry)
     end
 
-    def command_split
-      /([a-zA-Z0-9]+){1} (.*) ((?:&{2}|\|{1}))/
+    def command_split(entry)
+      /([a-zA-Z0-9]+){1} (.*) ((?:&{2}|\|{1}))/.match(entry)
     end
 
     def parse
-      # Returning dummy data for testing
-      [
-        { command: 'cd',   flags: [], value: nil      },
-        { command: '&&',   flags: [], value: nil      },
-        { command: 'ls',   flags: [], value: nil      },
-        { command: '|',    flags: [], value: nil      },
-        { command: 'grep', flags: [], value: 'foobar' }
-      ]
+
+      parsed_input = []
+
+      while input
+        command = command_split input
+        # build new array off the matched data to push to parsed input
+        # get the complete string from the matched data
+        # replace that matched string with nil in the input string
+        # the end case is the input string being nil or not matching any methods any more
+          # That will eventually return an exception
+      end
+
+      parsed_input
     end
 
   end
